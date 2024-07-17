42b689af3ce98147e1f25814e9773679097f2842
Pablo_Herrera <pabloemi1998@gmail.com>
2024-07-16 23:12:10 -0500

    Test EndPoints GET

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -11,11 +11,14 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   const todo1 = { text: 'Hola mundo 1' };
   const todo2 = { text: 'Hola mundo 2' };
 
   test('should return TODOs api/todos', async () => {
-    await prisma.todo.deleteMany();
     await prisma.todo.createMany({
       data: [todo1, todo2],
     });
@@ -23,7 +26,21 @@ describe('Todo route testing', () => {
     const { body } = await request(testServer.app).get('/api/todos').expect(200);
 
     expect(body).toBeInstanceOf(Array);
-    console.log(body);
-    // expect(body.lenght).toBe(2);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+    expect(body[0].completedAt).toBeNull();
+  });
+
+  test('should return TODOs api/todos/:id', async () => {
+    const todo = await prisma.todo.create({ data: todo1 });
+
+    const { body } = await request(testServer.app).get(`/api/todos/${todo.id}`).expect(200);
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo.text,
+      completedAt: todo.completedAt,
+    });
   });
 });
